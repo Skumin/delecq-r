24ac0ca09e77bee1897c7dc039f88b17b33ff67e
Skumin <35369581+Skumin@users.noreply.github.com>
2019-06-20 15:06:31 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,44 +1,44 @@
-gen_init_pop <- function(E, const, NP, boxbounds) {
-  if(!is.matrix(boxbounds)) {
-    stop("'boxbounds' must be a matrix.")
-  }
-  if(ncol(boxbounds) != 2) {
-    stop("'boxbounds' must have two columns.")
-  }
-  if(length(const) > 1 & !is.matrix(E)) {
-    stop("'E' must be a matrix for this constraints vector.")
-  }
-  if(length(const) == 1 & !is.matrix(E)) {
-    E <- t(as.matrix(E))
-  }
-  if(nrow(E) != length(const)) {
-    stop("The number of constraints must be equal to the number of rows of 'E'.")
-  }
-  M <- E %*% t(E)
-  if(length(const) == 1) {
-    y <- const/M
-    x0 <- t(E) %*% y
-    dm <- ncol(E)
-    xi <- matrix(0, ncol = dm, nrow = NP)
-    for(i in seq_len(NP)) {
-      boundsok <- FALSE
-      while(!boundsok) {
-        d <- as.matrix(runif(dm))
-        z <- E %*% d
-        u <- z/M
-        v <- t(E) %*% u
-        num <- x0 + d - v
-        if(all(sapply(seq_along(num), function(i) num[i] >= boxbounds[i, 1] & num[i] <= boxbounds[i, 2]))) {
-          xi[i, ] <- num
-          boundsok <- TRUE
-        }
-      }
-    }
-  } else {
-    stop("This is not yet implemented.")
-  }
-  return(xi)
-}
+# gen_init_pop <- function(E, const, NP, boxbounds) {
+#   if(!is.matrix(boxbounds)) {
+#     stop("'boxbounds' must be a matrix.")
+#   }
+#   if(ncol(boxbounds) != 2) {
+#     stop("'boxbounds' must have two columns.")
+#   }
+#   if(length(const) > 1 & !is.matrix(E)) {
+#     stop("'E' must be a matrix for this constraints vector.")
+#   }
+#   if(length(const) == 1 & !is.matrix(E)) {
+#     E <- t(as.matrix(E))
+#   }
+#   if(nrow(E) != length(const)) {
+#     stop("The number of constraints must be equal to the number of rows of 'E'.")
+#   }
+#   M <- E %*% t(E)
+#   if(length(const) == 1) {
+#     y <- const/M
+#     x0 <- t(E) %*% y
+#     dm <- ncol(E)
+#     xi <- matrix(0, ncol = dm, nrow = NP)
+#     for(i in seq_len(NP)) {
+#       boundsok <- FALSE
+#       while(!boundsok) {
+#         d <- as.matrix(runif(dm))
+#         z <- E %*% d
+#         u <- z/M
+#         v <- t(E) %*% u
+#         num <- x0 + d - v
+#         if(all(sapply(seq_along(num), function(i) num[i] >= boxbounds[i, 1] & num[i] <= boxbounds[i, 2]))) {
+#           xi[i, ] <- num
+#           boundsok <- TRUE
+#         }
+#       }
+#     }
+#   } else {
+#     stop("This is not yet implemented.")
+#   }
+#   return(xi)
+# }
 
 gen_init_pop_simple <- function(NP, boxbounds) {
   dm <- nrow(boxbounds)
@@ -150,7 +150,7 @@ run_delecq <- function(fun, ..., boxbounds, cr = 0.5, f.param = 0.5, maxgen = 50
 
 run_delecqC <- function(fun, ..., boxbounds, cr = 0.5, f.param = 0.5, maxgen = 500, NP = NULL, show.progress = TRUE) {
   gen <- 1
-  mat <- gen_init_pop_simple(NP = NP, boxbounds = boxbounds)
+  mat <- gen_init_popC(NP = NP, boxbounds = boxbounds)
   funvals <- apply(X = mat, MARGIN = 1, FUN = fun, ...)
   rbest <- which.max(funvals)
   trugen <- maxgen
