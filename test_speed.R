4e48a958a6cf78fdf60b4eacaa2323592256a253
Skumin <35369581+Skumin@users.noreply.github.com>
2019-06-28 16:51:27 +0100

    Update test_speed.R

--- test_speed.R
+++ test_speed.R
@@ -1,10 +1,10 @@
 library(Rcpp)
 
-sourceCpp('mutate.cc')
+sourceCpp('cpp_functions.cc')
 source('main.R')
 
 fn <- function(x) {
   -1*((x[1] - 2)^2 + (x[2] - 1)^2)
 }
 
-microbenchmark::microbenchmark(run_delecqC(fn, boxbounds = matrix(c(0, 0, 1, 1), nrow = 2), cr = 0.7, f.param = 0.9, maxgen = 50, show.progress = FALSE), run_delecq(fn, boxbounds = matrix(c(0, 0, 1, 1), nrow = 2), cr = 0.7, f.param = 0.9, maxgen = 50, show.progress = FALSE))
+microbenchmark::microbenchmark(run_deleq(fn, boxbounds = matrix(c(0, 0, 1, 1), nrow = 2), Emat = matrix(c(1, 1), nrow = 1), constr = matrix(1), cr = 0.7, f.param = 0.9, maxgen = 50, show.progress = FALSE), run_delecq(fn, boxbounds = matrix(c(0, 0, 1, 1), nrow = 2), cr = 0.7, f.param = 0.9, maxgen = 50, show.progress = FALSE))
